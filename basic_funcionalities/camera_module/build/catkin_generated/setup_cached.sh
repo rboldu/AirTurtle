968ab997d49e700d967ac20892c8ed47603016a6
Roger Boldu <roger.boldu@gmail.com>
2016-05-11 15:33:53 +0700

    basic functionalities structure

---
+++ basic_funcionalities/camera_module/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ahlab/AirTurtle/basic_funcionalities/camera_module/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/ahlab/AirTurtle/basic_funcionalities/camera_module/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ahlab/AirTurtle/basic_funcionalities/camera_module/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/ahlab/AirTurtle/basic_funcionalities/camera_module/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"

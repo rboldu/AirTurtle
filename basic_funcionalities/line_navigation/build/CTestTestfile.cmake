406fd941e52f05b93581847f324bf77ce8912b23
Roger Boldu <roger.boldu@gmail.com>
2016-05-11 15:22:36 +0700

    repering workspace

--- basic_funcionalities/line_navigation/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahlab/AirTurtle/basic_funcionalities/line_navigation/src
-# Build directory: /home/ahlab/AirTurtle/basic_funcionalities/line_navigation/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)

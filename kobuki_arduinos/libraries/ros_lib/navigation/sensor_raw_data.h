a2a50e99e3b826050782839660a8c376fe4fae9a
Roger Boldu <roger.boldu@gmail.com>
2016-05-23 17:00:31 +0700

    update msg navigation

--- kobuki_arduinos/libraries/ros_lib/navigation/sensor_raw_data.h
+++ kobuki_arduinos/libraries/ros_lib/navigation/sensor_raw_data.h
@@ -12,7 +12,7 @@ namespace navigation
   class sensor_raw_data : public ros::Msg
   {
     public:
-      uint8_t averagePosition;
+      int8_t averagePosition;
 
     sensor_raw_data():
       averagePosition(0)
@@ -22,7 +22,12 @@ namespace navigation
     virtual int serialize(unsigned char *outbuffer) const
     {
       int offset = 0;
-      *(outbuffer + offset + 0) = (this->averagePosition >> (8 * 0)) & 0xFF;
+      union {
+        int8_t real;
+        uint8_t base;
+      } u_averagePosition;
+      u_averagePosition.real = this->averagePosition;
+      *(outbuffer + offset + 0) = (u_averagePosition.base >> (8 * 0)) & 0xFF;
       offset += sizeof(this->averagePosition);
       return offset;
     }
@@ -30,13 +35,19 @@ namespace navigation
     virtual int deserialize(unsigned char *inbuffer)
     {
       int offset = 0;
-      this->averagePosition =  ((uint8_t) (*(inbuffer + offset)));
+      union {
+        int8_t real;
+        uint8_t base;
+      } u_averagePosition;
+      u_averagePosition.base = 0;
+      u_averagePosition.base |= ((uint8_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      this->averagePosition = u_averagePosition.real;
       offset += sizeof(this->averagePosition);
      return offset;
     }
 
     const char * getType(){ return "navigation/sensor_raw_data"; };
-    const char * getMD5(){ return "f80e0dc5f527e8deac131616f6c00d42"; };
+    const char * getMD5(){ return "d206a64c7d7498ae43986f17623d9c0b"; };
 
   };
 

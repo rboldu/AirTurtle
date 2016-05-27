3c6099be6dbb7a1afad29017ed770fb0d6ddc6c9
Roger Boldu <roger.boldu@gmail.com>
2016-05-27 17:53:29 +0800

    first version line follower, just tested on the simulator

--- src/basic_functionalities/navigation/navigation/src/followLine.py
+++ src/basic_functionalities/navigation/navigation/src/followLine.py
@@ -22,31 +22,77 @@ ENDC = '\033[0m'
 FAIL = '\033[91m'
 OKGREEN = '\033[92m'
 
+MAX_SPEED=0.05
+DESIRED_VALUE_SENSOR=0
+SPEED_DEPENDENCE=0.3
+
 '''
 @this is a navigation state
 @The main Idea is to follow the the line that is on the floor
 @/cmd_velux/input/navi
 '''
+class pid():
+    def __init__(self,Kp=1,Ki=2,Kd=3,iteration_time=0.5):
+        self.error=0
+        self.error_prior=0
+        self.integral=0
+        self.Kp=Kp
+        self.Ki=Ki
+        self.Kd=Kd
+        self.iteration_time=iteration_time
+        self.derivative=0
+
+    def calculateOutput(self,linePosition):
+     
+        error=DESIRED_VALUE_SENSOR-linePosition
+        self.intergal=self.integral+(error*self.iteration_time)
+        self.derivative=(error - self.error_prior)/self.iteration_time
+        output=self.Kp*error+self.Ki*self.integral+self.Kd*self.derivative
+        self.error_prior=error
+
+        msg=Twist()
+        speed=MAX_SPEED-abs(output)*SPEED_DEPENDENCE
+        if speed <0 :
+            speed=0
+        msg.linear.x= speed
+        msg.linear.y=0
+        msg.linear.z=0
+        msg.angular.x=0
+        msg.angular.y=0
+        msg.angular.z=output
+
+        return msg
+        
+
+
+
+class lineSensorFollow():
+
+    def __init__(self):
+        self.average=0
+        self.time=0
+        self.lineSensor_subs = rospy.Subscriber("navigation/sensor/line_possition", sensor_raw_data, self.updateValue)
+
+    def print_line(self):
+        print self.average
+
+    def updateValue(self,data):
+        self.average=data.averagePosition
+        self.time=rospy.get_rostime()
+        self.print_line()
+    def gedAverage(self):
+        return self.Sensor.average
+
 
 class navigation_followLine():
     
     def __init__(self):
-        rospy.loginfo("Initializing reverse")
-        
-        self.nav_pub= rospy.Publisher('/cmd_velux/input/navi', Twist)
-	self.lineSensor_subs = rospy.Subscriber("navigation/sensor/line_possition", sensor_raw_data, self.print_line)
-        self.average_possition=0
+        rospy.loginfo("Initializing line follower")
+        self.nav_pub= rospy.Publisher('/cmd_vel_mux/input/navi', Twist)
 
-    def print_line(self,data):
-        self.line_data=data
-	if self.line_data.averagePosition >0 :
-		self.pub_move()
-	print self.line_data
-        
-            
     def pub_move(self):
         msg=Twist()
-        msg.linear.x= 10
+        msg.linear.x= 0.5
         msg.linear.y=0
         msg.linear.z=0
         msg.angular.x=0
@@ -61,13 +107,20 @@ class navigation_followLine():
         msg.linear.z=0
         msg.angular.x=0
         msg.angular.y=0
-        msg.angular.z=0
+        msg.angular.z=0.0
 
         self.nav_pub.publish(msg)
        
     def run(self):
+        line=lineSensorFollow()
+        pidFollow=pid(Kp=0.0009,Ki=0.0005,Kd=0.0005,iteration_time=3)
         while not rospy.is_shutdown():
-            rospy.sleep(3)
+            msg=pidFollow.calculateOutput(line.average)
+            self.nav_pub.publish(msg)
+
+
+
+            rospy.sleep(0.3)
         
 if __name__ == '__main__':
     rospy.init_node('Following_Line_server')
